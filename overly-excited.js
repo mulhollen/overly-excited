3f22f2d178542369fb033645e3f5ac5cf1fbf376
Lindsay Mulhollen <lindsaymulhollen@Lindsays-MacBook-Pro.local>
2018-01-29 15:54:46 -0600

    I did it

--- overly-excited.js
+++ overly-excited.js
@@ -1,43 +1,30 @@
 
-// Create an array that contains the words in the sentence
+
 let sentence = ["The", "walrus", "danced", "through", "the", "trees", "in", "the", "light", "of", "the", "moon"];
 let betterSentence = ["Lindsay", "is", "astonished", "how", "little", "JavaScript", "this", "exercise", "takes"];
 
-/*
-    The addExcitement function should be an impure function, and accept
-    the array as the sole argument. It should iterate over the array
-    and output the words to the browser console.
-*/
 
-
-
-function addExcitement(theWordArray, punctuation) {
-   console.log(theWordArray, punctuation);
-    
+// holds the word you are adding and sentence
 var counter = "";
 
-theWordArray.forEach(function(item, index)
-    {
-        var idx = index + 1;
-        
-        // add logic to addExcitement that places an exclamation point(!) after every third word.
+function addExcitement(theWordArray, punctuation) {
+    
+    theWordArray.forEach(function(item, index)
+        {
+            var idx = index + 1; //makes the array easily divisable by 3
 
-        if (idx % 3 === 0) {
-            let x = idx / 3;
-            counter += " " + item + punctuation.repeat(x);
-        } else {
-            counter += " " + item;
-        }
-        // console.log(counter);
+            if (idx % 3 === 0) { //picks 3rd word
+                let x = idx / 3; // x gives the number of times the punctuation should be repeated
+                counter += ` ${item + punctuation.repeat(x)}`; // uses this structure to output to dom += does not like extra +'s
+            } else {
+                counter += ` ${item}`;
+            }
+        console.log(counter); //outputs to the console
+
+        document.getElementById("sentence").innerHTML += `${counter} <br>`; //outputs to the dom
         
-    })
+    });
 }
 
-console.log(addExcitement(sentence, "!"));
+addExcitement(sentence, "!") //envokes function to run with this word array and puncation
 
-
-
-// Add a new argument to the function so that a developer can specify which character should be displayed instead of it always being an exclamation point.
-
-
-// Invoke the function and pass in the array
